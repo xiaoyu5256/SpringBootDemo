8ce2174e4b9bbd3cf3aff6478b980735ca2b0ba7
xiaoyu5256 <lanfeng5256@163.com>
2016-07-09 18:23:45 +0800

    use lombok

--- src/main/java/cn/showmecode/demo/springboot/domain/User.java
+++ src/main/java/cn/showmecode/demo/springboot/domain/User.java
@@ -5,30 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String username;
 	private String password;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
 	
 }
