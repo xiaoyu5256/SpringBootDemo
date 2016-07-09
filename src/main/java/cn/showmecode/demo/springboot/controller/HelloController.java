7738b520e67a89c0d798c0fb5f0f1615078416d9
xiaoyu5256 <lanfeng5256@163.com>
2016-07-09 23:36:38 +0800

    使用devtools自动重启

--- src/main/java/cn/showmecode/demo/springboot/controller/HelloController.java
+++ src/main/java/cn/showmecode/demo/springboot/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import cn.showmecode.demo.springboot.dao.UserDao;
+import cn.showmecode.demo.springboot.domain.UserRepository;
 
 @RestController
 @RequestMapping("/")
@@ -14,7 +14,7 @@ public class HelloController {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String hello() {
-		return "Hello Spring Boot!";
+		return "Hello Spring Boot!ss";
 	}
 	
 }
--- src/main/java/cn/showmecode/demo/springboot/controller/UserController.java
+++ src/main/java/cn/showmecode/demo/springboot/controller/UserController.java
@@ -11,33 +11,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import cn.showmecode.demo.springboot.dao.UserDao;
 import cn.showmecode.demo.springboot.domain.User;
+import cn.showmecode.demo.springboot.domain.UserRepository;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	private final static Integer PAGE_SIZE = 2;
 	@Autowired
-	private UserDao userDao;
+	private UserRepository userRepository;
 	
 	@RequestMapping(value="/pages/{pagenum}",method=RequestMethod.GET )
 	public Page<User> listUsers(@PathVariable Integer pagenum){
 		PageRequest pageable = new PageRequest(pagenum - 1, PAGE_SIZE);
-		return userDao.findAll(pageable);
+		return userRepository.findAll(pageable);
 	}
 	@RequestMapping(method=RequestMethod.GET )
 	public Page<User> queryUsers(Pageable pageable){
-		Page<User> users = userDao.findAll(pageable);
+		Page<User> users = userRepository.findAll(pageable);
 		return users;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST )
 	public List<User> updateUsers(User user){
-		if(user!=null&&userDao.findByUsername(user.getUsername())!=null){
+		if(user!=null&&userRepository.findByUsername(user.getUsername())!=null){
 			return null;
 		}
-		user = userDao.save(user);
-		return userDao.findAll();
+		user = userRepository.save(user);
+		return userRepository.findAll();
 	}
 }
